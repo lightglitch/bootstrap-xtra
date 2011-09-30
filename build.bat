6e94d2b04d872030035373cbf84df2eebd941265
lightglitch <lightglitch@gmail.com>
2011-09-30 14:07:43 +0100

    Fixed build process

--- build.bat
+++ build.bat
@@ -1,4 +1,8 @@
+call .\build\BatchSubstitute.bat "@VERSION" "1.3.0" .\lib\bootstrap.less > .\lib\bootstrap.tmp.less
+call .\build\BatchSubstitute.bat "@DATE" "%date% %time%" .\lib\bootstrap.tmp.less > .\lib\bootstrap.tmp.1.less
 cd lib
-@cscript //nologo "../build/lessc.wsf" bootstrap.less ../bootstrap.css
-@cscript //nologo "../build/lessc.wsf" bootstrap.less ../bootstrap.min.css -compress
-cd ../
+@cscript //nologo "../build/lessc.wsf" bootstrap.tmp.1.less ../bootstrap.css
+@cscript //nologo "../build/lessc.wsf" bootstrap.tmp.1.less ../bootstrap.min.css -compress
+del /f bootstrap.tmp.less
+del /f bootstrap.tmp.1.less
+cd ..
