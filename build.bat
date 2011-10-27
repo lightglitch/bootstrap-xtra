e6642c85e1c4e9072b17ab0aada9580513e048dd
lightglitch <lightglitch@gmail.com>
2011-10-27 15:41:53 +0100

    Refactor code so that would be separated from official version

--- build.bat
+++ build.bat
@@ -1,8 +1,8 @@
-call .\build\BatchSubstitute.bat "@VERSION" "1.3.0" .\lib\bootstrap.less > .\lib\bootstrap.tmp.less
-call .\build\BatchSubstitute.bat "@DATE" "%date% %time%" .\lib\bootstrap.tmp.less > .\lib\bootstrap.tmp.1.less
+call .\build\BatchSubstitute.bat "@VERSION" "1.3.0" .\lib\bootstrap_xtra.less > .\lib\bootstrap_xtra.tmp.less
+call .\build\BatchSubstitute.bat "@DATE" "%date% %time%" .\lib\bootstrap_xtra.tmp.less > .\lib\bootstrap_xtra.tmp.1.less
 cd lib
-@cscript //nologo "../build/lessc.wsf" bootstrap.tmp.1.less ../bootstrap.css
-@cscript //nologo "../build/lessc.wsf" bootstrap.tmp.1.less ../bootstrap.min.css -compress
-del /f bootstrap.tmp.less
-del /f bootstrap.tmp.1.less
+@cscript //nologo "../build/lessc.wsf" bootstrap_xtra.tmp.1.less ../bootstrap.css
+@cscript //nologo "../build/lessc.wsf" bootstrap_xtra.tmp.1.less ../bootstrap.min.css -compress
+del /f bootstrap_xtra.tmp.less
+del /f bootstrap_xtra.tmp.1.less
 cd ..
